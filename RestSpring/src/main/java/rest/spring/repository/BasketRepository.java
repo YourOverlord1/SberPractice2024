68722c5346454688e431b924cff540f33946261c
YourOverlord1 <denissakolov2016@gmail.com>
2024-07-17 19:14:37 +0300

    Домашка rest

--- RestSpring/src/main/java/rest/spring/repository/BasketRepository.java
+++
@@ -1,9 +0,0 @@
-package rest.spring.repository;
-
-import rest.spring.model.Client;
-
-public interface BasketRepository {
-    long addBasket();
-
-    void delitBasket(Client client);
-}
--- RestSpring/src/main/java/rest/spring/service/BasketService.java
+++
@@ -1,9 +0,0 @@
-package rest.spring.service;
-
-import rest.spring.model.Client;
-
-public interface BasketService {
-    public void addBasket();
-
-    public void delitBasket(Client client);
-}
